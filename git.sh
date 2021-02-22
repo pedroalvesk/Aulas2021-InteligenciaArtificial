a9bf0439cf5f6cf7aae2d9e77b97511bcafe6e74
Pedro Alves <pedroalvesk@protonmail.com>
2021-02-22 21:08:36 +0000

    Removed git file

--- git.sh
+++
@@ -1,6 +0,0 @@
-git init
-git remote add origin https://github.com/pedroalvesk/Aulas2021-InteligenciaArtificial.git
-git checkout -b main
-git pull origin main
-git add *
-git commit -am "First Commit"
